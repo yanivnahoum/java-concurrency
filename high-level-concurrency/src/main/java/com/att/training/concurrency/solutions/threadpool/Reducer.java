4acc1ac3cd004ba22cfa13d19fad19ef60fab2a4
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 17:02:18 +0300

    [high-level] Corrected collection in Reducer.java

--- high-level-concurrency/src/main/java/com/att/training/concurrency/solutions/threadpool/Reducer.java
+++ high-level-concurrency/src/main/java/com/att/training/concurrency/solutions/threadpool/Reducer.java
@@ -2,8 +2,8 @@ package com.att.training.concurrency.solutions.threadpool;
 
 import com.att.training.concurrency.exercises.common.CalculatorService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
@@ -11,7 +11,7 @@ class Reducer {
 
     private final CalculatorService calculatorService;
     private final ExecutorService executorService;
-    private final List<Future<Long>> tasks = new ArrayList<>();
+    private final Collection<Future<Long>> tasks = new ConcurrentLinkedDeque<>();
 
     Reducer(CalculatorService calculatorService, ExecutorService executorService) {
         this.calculatorService = calculatorService;
